05cc060ceb4c77e3c5045b00b4959a43a50d1124
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2016-07-29 00:18:56 +0900

    implement connection buffer to reduce extra calling of send(2) call

--- include/stomp/connection.h
+++ include/stomp/connection.h
@@ -1,15 +1,20 @@
 #ifndef __CONNECTION_H__
 #define __CONNECTION_H__
 
+#include <stomp/list.h>
+#include <pthread.h>
+
 #define QUEUELEN (1<<16)
+#define CONNECTION_BUFFER_SIZE (1<<22)
 
 typedef struct connection_t {
   int sock;
+  int buffer_index;
+  char *buffer;
 } connection_t;
 
-connection_t *conn_init(char *, int);
-
 int conn_send(connection_t *, char *, int);
+connection_t *conn_init(char *, int);
 void conn_cleanup(connection_t *);
 
 #endif
